a44fdd08a492e7eed2120a951f752e5b249ace6d
sumi419 <sasgar04@gmail.com>
2019-04-24 23:33:18 -0700

    ❤️ Convert tabs to spaces.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-	return (
-		<div className='App'>
-			<div>hello</div>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <div>hello</div>
+    </div>
+  );
 }

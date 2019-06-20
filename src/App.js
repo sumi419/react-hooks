59847167638ee7dcf5f101ed1dce5d6993118774
sumi419 <sasgar04@gmail.com>
2019-06-19 23:24:21 -0700

    💕 import Hooks2 into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Hooks from './components/Hooks';
+import Hooks2 from './components/Hooks2';
 
 import './App.css';
 
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <div className='App'>
       <div>hello</div>
-      <Hooks />
+      <Hooks2 />
     </div>
   );
 }
